5ade46c1f3c0dad8010858710adcb2b4f75c204e
leastburg <leastburg@gmail.com>
2023-02-09 11:39:22 -0800

    clean up print commands

--- main.py
+++ main.py
@@ -14,15 +14,17 @@ def main():
         print(f"The '{item[0]}' character was found '{item[1]}' times")
 
 
-
+# this retrieves the book text file you would like the program to examine and converts it into one long string
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+# this turns a string into an array of words, and counts the number of words
 def get_num_words(string):
     words_list = string.split()
     return len(words_list)
 
+# this makes a dictionary with a count of how many times each character from the alphabet occurs in a string
 def get_num_letters(string):
     letter_count = {}
     for character in string:
@@ -33,9 +35,9 @@ def get_num_letters(string):
             letter_count[lowered] = 1
     return letter_count
 
+# this turns a dictionary into a sorted list of tuples, sorted by the value rather than the key, in descending order
 def dictionary_to_list(dictionary):
     sorted_list = list(dictionary.items())
-    print(sorted_list)
     sorted_list.sort(key = lambda i:i[1], reverse = True)
     return sorted_list
     
