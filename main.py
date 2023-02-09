6bb7ad7c25c8478eb59016865dd012eb1b5bb014
leastburg <leastburg@gmail.com>
2023-02-08 17:31:35 -0800

    add functions to open book file and read to string

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+book_path = "books/frankenstein.txt"
+
+def main():
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
