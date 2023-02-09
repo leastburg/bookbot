3b5618460241bb1128921795f6312c6e316c21fd
leastburg <leastburg@gmail.com>
2023-02-08 17:47:43 -0800

    add function to split string to list and return word count

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ book_path = "books/frankenstein.txt"
 
 def main():
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    print(num_words)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_num_words(string):
+    words_list = string.split()
+    return len(words_list)
 
 main()
