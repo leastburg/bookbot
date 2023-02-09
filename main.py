2375c1fe813a41a214ad8c133757f7fef677f099
leastburg <leastburg@gmail.com>
2023-02-08 18:37:21 -0800

    add function to return dictionary of character quantities

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(num_words)
+    chars_dict = get_num_letters(text)
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,5 +14,16 @@ def get_book_text(path):
 def get_num_words(string):
     words_list = string.split()
     return len(words_list)
+
+def get_num_letters(string):
+    letter_count = {}
+    for character in string:
+        lowered = character.lower()
+        if lowered in letter_count:
+            letter_count[lowered] += 1
+        elif lowered not in letter_count:
+            letter_count[lowered] = 1
+    return letter_count
+    
 
 main()
